aa57ec8020f83bb21b99fb58c077fba7b7eb1477
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2025-03-12 14:23:25 +0530

    [Praveen] ADD : Library Management System

--- LibraryManagement.java
+++ LibraryManagement.java
@@ -6,7 +6,7 @@ public class LibraryManagement {
         final String isbn; // Final variable to ensure unique identifier
 
         // Constructor using 'this' to initialize attributes
-        public Book(String title, String author, String isbn) {
+        public Book(String title, String author, String isbn) { 
             this.title = title;
             this.author = author;
             this.isbn = isbn;
